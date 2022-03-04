8a4011e4b73ebdb36c3161cbc1bde14a0f30b76c
Juliano Martins <juliano.martins@openk.com.br>
2022-03-04 10:55:30 -0300

    universidade

---
+++ Models/AlunoModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Universidade.Models
+{   
+    public class AlunoModel
+    {
+        public int id { get; set;}
+        public string nome { get; set; }
+        public DateTime Data_nascimento { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+        public string cpf { get; set; }    
+    }
+}
